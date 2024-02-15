989d613b62f34ffd5b532578208d40c271502fca
MINDLAB1 <98901049+MINDLAB1@users.noreply.github.com>
2024-02-15 14:37:00 -0800

    Delete training_single_GPU.sh

--- training_single_GPU.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --ntasks=16 #this only affects MPI job
-#SBATCH --time=18:00:00
-
-module load anaconda3
-source activate python_38
-python3 main_downstream.py
